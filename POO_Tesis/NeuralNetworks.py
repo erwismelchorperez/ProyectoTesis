3e54becc3d894a346c4ef4d827a27cd1793f3e4f
emelchor <melchor.erwis@gmail.com>
2022-08-27 23:06:56 -0500

    first commit

---
+++ POO_Tesis/NeuralNetworks.py
@@ -0,0 +1,12 @@
+"""
+NeuralNetworks.py
+Autor: Erwis Melchor Pérez
+Maestría en Tecnologías de Cómputo Aplicado
+Tesis
+Tercer Semestre
+"""
+class NeuralNetworks:
+    def __init__(self,dataset):
+        self.hidden_layer_one = 20
+        self.hidden_layer_two = 15
+        self.hidden_layer_three = 12
