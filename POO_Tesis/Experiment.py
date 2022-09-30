6ea036d721515cf77cb046351cacc178e6802eee
emelchor <melchor.erwis@gmail.com>
2022-09-30 12:24:59 -0500

    cambios módulos

--- POO_Tesis/Experiment.py
+++ POO_Tesis/Experiment.py
@@ -41,18 +41,18 @@ class Experiment:
         if method == "RNN":
             rnn = NeuralNetworks()
             rnn.WriteLogger("RNN Busqueda en Malla 2 Neuronas")
-            """
             capaOculta1 = [40, 30, 20]
             capaOculta2 = [25, 20, 15]
-            capaOculta3 = [12,  8,  4]
-            """
+            capaOculta3 = [12,  8,  4]            
             funcionactivacion1 = ['tanh','sigmoid','relu','softmax','selu']
             funcionactivacion2 = ['tanh','sigmoid','relu','softmax','selu']
             funcionactivacion3 = ['tanh','sigmoid','relu','softmax','selu']
             funcionactivacion4 = ['tanh','sigmoid','relu','softmax','selu']
+            """
             capaOculta1 = [40, 30, 20, 12, 15, 25, 35, 50, 45, 18, 22, 24, 32, 36, 38, 44, 66, 88]
             capaOculta2 = [25, 20, 15,  6,  8, 10, 14, 16, 18, 22, 24, 30, 44, 40, 66, 56, 52, 28]
             capaOculta3 = [12,  8,  4,  3,  2,  6, 10, 14, 20, 22, 26, 32, 24, 46, 40, 62, 48, 88]
+            """
 
             mejor_red_encontrada = rnn.SearchMesh(capaOculta1,capaOculta2,capaOculta3,self.getDataset(),2500,funcionactivacion1,funcionactivacion2,funcionactivacion3,funcionactivacion4)#retorna la mejor red encontrada
             rnn.EvaluateBestNetwork(mejor_red_encontrada[0],mejor_red_encontrada[1],mejor_red_encontrada[2],self.getDataset(),2500)#2 neuronas capa salida
